eb366ee0b90ed33bee26bd825251a953bbf83958
kondo0602 <kondo8363@gmail.com>
2024-02-11 19:01:32 +0900

    Add custom colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
 	content: ["./src/**/*.{astro,html,md,mdx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+	  			primary: "#e2e8f0", // slate-200
+	  			secondary: "#64748b", // slate-500
+					background: "#020617" // slate-950
+      },
+    },
 	},
 	plugins: [],
 };
