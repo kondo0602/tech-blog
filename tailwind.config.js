d15d1f752f008f881ff731114104a20cbf51e82b
kondo0602 <kondo8363@gmail.com>
2024-02-24 14:52:23 +0900

    Add link to TimelineItem and update body background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ export default {
       colors: {
         primary: "#e2e8f0", // slate-200
         secondary: "#94a3b8", // slate-400
-        background: "#020617", // slate-950
+        backgroundPale: "#1e293b", // slate-700
+        backgroundBase: "#020617", // slate-950
         accent: "#f59e0b", // yellow-500
       },
     },
